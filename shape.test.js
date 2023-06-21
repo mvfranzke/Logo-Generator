d0b334421acc7cf9af7eb1033d626a32eb3182da
mvfranzke <michellevonfranzke@gmail.com>
2023-06-21 16:18:49 -0700

    input test for triangle

--- shape.test.js
+++ shape.test.js
@@ -1,8 +1,14 @@
 //import shape js
-
+const { exportSpecifier } = require('@babel/types');
+const { Triangle, Circle, Square} = require ('./shape');
 
 //test for triangle
-
+test ('Triangle should render SVG content', () => {
+  const shape = new Triangle();
+  shape.setColor('blue');
+  expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill = "blue"/>');
+}
+);
 
 //text for circle
 
